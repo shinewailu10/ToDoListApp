444fcac61c0c2195c85852aaf603453898ca4231
Shine Wai Lu <120560614+shinewailu10@users.noreply.github.com>
2024-06-12 12:58:41 +0800

    new project created
    create new project for ToDoListApp

---
+++ ToDoList/ToDoList/ContentView.swift
@@ -0,0 +1,18 @@
+//
+//  ContentView.swift
+//  ToDoList
+//
+//  Created by Shine Wai Lu on 12/6/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        
+    }
+}
+
+#Preview {
+    ContentView()
+}
