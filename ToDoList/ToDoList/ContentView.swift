671e1ce7e34c3a01371c597a4b1c39de1a49382f
Shine Wai Lu <shine.wai.lu10@gmail.com>
2024-06-12 14:57:31 +0800

    text alignment

--- ToDoList/ToDoList/ContentView.swift
+++ ToDoList/ToDoList/ContentView.swift
@@ -27,7 +27,7 @@ struct ContentView: View {
                         Text("My ToDo List")
                             .font(.largeTitle)
                             .padding()
-                        Spacer()
+                        
                     }
 
                 }
